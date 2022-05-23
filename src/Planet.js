e3b6e198882342253cd10ca4c1884914a5336330
jayG9269 <hs9269@gmail.com>
2022-05-23 23:59:09 +0900

    add rotation

--- src/Planet.js
+++ src/Planet.js
@@ -1,26 +1,48 @@
-import * as THREE from 'three';
+// import * as THREE from 'three';
+
+// export class Planet {
+// 	constructor(info) {
+// 		this.x = info.x;
+// 		this.ratio = 0.001*info.ratio
+
+// 		info.gltfLoader.load(
+// 			info.modelSrc,
+// 			glb => {
+// 				this.mesh = glb.scene.children[0];
+
+// 				this.mesh.castShadow = true;
+// 				this.mesh.position.set(this.x, 0, 0);
+
+
+// 				this.mesh.scale.set(this.ratio, this.ratio, this.ratio)
+// 				info.scene.add(this.mesh);
+
+// 				// const box = new THREE.Box3().setFromObject(this.mesh);
+// 				// const size = box.getSize();
+// 				// console.log(size)
+// 			}
+// 		);
+// 	}
+// }
+
+
+import * as THREE from "three";
 
 export class Planet {
-	constructor(info) {
-		this.x = info.x;
-		this.ratio = 0.001*info.ratio
+  constructor(radius, positionX, textureFile) {
+    this.radius = radius;
+    this.positionX = positionX;
+    this.textureFile = textureFile;
+  }
 
-		info.gltfLoader.load(
-			info.modelSrc,
-			glb => {
-				this.mesh = glb.scene.children[0];
-
-				this.mesh.castShadow = true;
-				this.mesh.position.set(this.x, 0, 0);
-
-
-				this.mesh.scale.set(this.ratio, this.ratio, this.ratio)
-				info.scene.add(this.mesh);
-
-				const box = new THREE.Box3().setFromObject( this.mesh );
-				const size = box.getSize();
-				// console.log(size)
-			}
-		);
-	}
+  getMesh() {
+    if (this.mesh === undefined || this.mesh === null) {
+      const geometry = new THREE.SphereGeometry(this.radius);
+      const texture = new THREE.TextureLoader().load(this.textureFile);
+      const material = new THREE.MeshBasicMaterial({ map: texture });
+      this.mesh = new THREE.Mesh(geometry, material);
+      this.mesh.position.x += this.positionX;
+    }
+    return this.mesh;
+  }
 }
--- src/main.js
+++ src/main.js
@@ -23,12 +23,12 @@ const scene = new THREE.Scene();
 
 // Camera
 const camera = new THREE.PerspectiveCamera(
-	100,
+	75,
 	window.innerWidth / window.innerHeight,
 	0.1,
-	100000
+	1000
 );
-camera.position.set(0, 0, 10);
+camera.position.set(0, 50, 100);
 camera.lookAt(0, 0, 0)
 scene.add(camera);
 
@@ -40,14 +40,14 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const ambientLight = new THREE.AmbientLight('white', 1);
 scene.add(ambientLight);
 
-const spotLight = new THREE.SpotLight('white', 0.7);
-spotLight.position.set(0, 0, 0);
-spotLight.castShadow = true;
-spotLight.shadow.mapSize.width = 1024;
-spotLight.shadow.mapSize.height = 1024;
-spotLight.shadow.camera.near = 1;
-spotLight.shadow.camera.far = 200;
-scene.add(spotLight);
+// const spotLight = new THREE.SpotLight('white', 0.7);
+// spotLight.position.set(0, 0, 0);
+// spotLight.castShadow = true;
+// spotLight.shadow.mapSize.width = 1024;
+// spotLight.shadow.mapSize.height = 1024;
+// spotLight.shadow.camera.near = 1;
+// spotLight.shadow.camera.far = 200;
+// scene.add(spotLight);
 
 // const lightHelper = new THREE.SpotLightHelper(spotLight);
 // scene.add(lightHelper);
@@ -63,17 +63,55 @@ const gltfLoader = new GLTFLoader();
 // floorMesh.receiveShadow = true;
 // scene.add(floorMesh);
 
-const sun = new Planet({ gltfLoader, scene, modelSrc: '/obj/Sun.glb', x: 0, ratio: 109});
-const mercury = new Planet({ gltfLoader, scene, modelSrc: '/obj/Mercury.glb', x: 4, ratio: 0.38});
-const venus = new Planet({ gltfLoader, scene, modelSrc: '/obj/Venus.glb', x: 7, ratio: 0.95});
-const earth = new Planet({ gltfLoader, scene, modelSrc: '/obj/EarthClouds.glb', x: 10, ratio: 1});
-const mars = new Planet({ gltfLoader, scene, modelSrc: '/obj/Mars.glb', x: 15, ratio: 0.52});
+// const sun = new Planet({ gltfLoader, scene, modelSrc: '/obj/Sun.glb', x: 0, ratio: 109});
+// const mercury = new Planet({ gltfLoader, scene, modelSrc: '/obj/Mercury.glb', x: 4, ratio: 0.38});
+// const venus = new Planet({ gltfLoader, scene, modelSrc: '/obj/Venus.glb', x: 7, ratio: 0.95});
+// const earth = new Planet({ gltfLoader, scene, modelSrc: '/obj/EarthClouds.glb', x: 10, ratio: 1});
+// const mars = new Planet({ gltfLoader, scene, modelSrc: '/obj/Mars.glb', x: 15, ratio: 0.52});
+
+
+const sun = new Planet(8, 0, "/obj/sun.jpeg").getMesh();
+scene.add(sun)
+
+const mercury = new Planet(2, 20, "/obj/mercury.png").getMesh();
+const mercuryGroup = new THREE.Group()
+mercuryGroup.add(mercury)
+scene.add(mercuryGroup)
+
+const venus = new Planet(3, 35, "/obj/venus.jpeg").getMesh();
+const venusGroup = new THREE.Group()
+venusGroup.add(venus)
+scene.add(venusGroup)
+
+const earth = new Planet(4, 50, "/obj/earth.jpeg").getMesh();
+const earthGroup = new THREE.Group()
+earthGroup.add(earth)
+scene.add(earthGroup)
+
+const mars = new Planet(3, 75, "/obj/mars.jpeg").getMesh();
+const marsGroup = new THREE.Group()
+marsGroup.add(mars)
+scene.add(marsGroup)
+
 
 // 그리기
 const clock = new THREE.Clock();
 
 function draw() {
 	const delta = clock.getDelta();
+
+	// sunGroup.rotation.y += delta
+	mercuryGroup.rotation.y += delta / 2.4
+	venusGroup.rotation.y += delta / 6.2
+	earthGroup.rotation.y += delta / 10
+	marsGroup.rotation.y += delta / 19
+	
+	sun.rotation.y += delta / 0.7
+	mercury.rotation.y += delta / 1.6
+	venus.rotation.y += delta / 6.7
+	earth.rotation.y += delta / 0.02
+	mars.rotation.y += delta / 0.02
+	
 
 	renderer.render(scene, camera);
 	renderer.setAnimationLoop(draw);
